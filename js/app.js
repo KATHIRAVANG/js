cc996140c3e45d5705d2536d5f0d85b6c3e8cd94
kathir <gkathiravan@gmail.com>
2018-12-22 22:02:57 +0530

    ext3

--- js/app.js
+++ js/app.js
@@ -9,6 +9,22 @@ app.init = function () {
 
     app.element = document.getElementById("btn1");
 
-    app.element.onclick = app.showMessage;
+    app.element.onclick = app.printAllObject;
 
 };
+
+app.printAllObject = function () {
+
+    var obj = window;
+
+    do Object.getOwnPropertyNames(obj).forEach(function (name) {
+        console.log(name);
+    });
+    while (obj = Object.getPrototypeOf(obj));
+}
+
+app.oldwayPrintAllObject = function () {
+    for (var key in window) {
+        console.log(key);
+    }
+}
